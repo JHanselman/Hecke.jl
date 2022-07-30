5b89fcf1552d8128b0e57882782056d36e736654
jeroen <jeroen@debian.jeroenthuis.edu>
2022-07-30 23:52:59 +0200

    Working on paths

--- src/RieSrf/RiemannSurface.jl
+++ src/RieSrf/RiemannSurface.jl
@@ -6,7 +6,7 @@
 #
 ################################################################################
 
-export RieSrf
+export RieSrf, CPath
 
 export RiemannSurface, discriminant_points, embedding, genus, precision
 
@@ -45,7 +45,40 @@ mutable struct RiemannSurface
     
     return RS
   end
+end
 
+mutable struct CPath
+
+  path_type::Int
+  start_point::acb
+  end_point::acb
+  
+  
+  center::acb
+  radius::arb
+  counterclockwise::Bool
+  
+  length::arb
+  
+  #Path type index:
+  #0 is a line
+  
+  function CPath(start_point::acb, end_point::acb, path_type::Int, center::acb = zero(parent(start_point)), radius::arb = zero(parent(start_point)), counterclockwise::Bool = true)
+  
+    P = new()
+    P.start_point = start_point
+    P.end_point = end_point
+    P.path_type = path_type
+    P.center = center
+    P.radius = radius
+    P.counterclockwise = counterclockwise
+    return P
+  end
+
+end
+
+function c_line(start_point::acb, end_point::acb)
+  return CPath(start_point, end_point, 0)
 end
 
 function defining_polynomial(RS::RiemannSurface)
@@ -66,27 +99,132 @@ function genus(RS::RiemannSurface)
   return RS.genus
 end
 
-function embedding(RS)
+function embedding(RS::RiemannSurface)
   return RS.embedding
 end
 
-function small_period_matrix(RS)
+function small_period_matrix(RS::RiemannSurface)
   return tau
 end
 
-function precision(RS)
+function precision(RS::RiemannSurface)
   return RS.prec
 end
 
-function fundamental_group(RS::RiemannSurface)
+function max_radius(RS::RiemannSurface)
+  return 1//4
+end
+
+#Follows algorithm 4.3.1 in Neurohr
+function fundamental_group(RS::RiemannSurface, abel_jacobi::Bool = false)
+  
+  #Compute the exceptional values x_i
   D_points = discriminant_points(RS)
+  d = length(D_points)
+  Cc = parent(D_points[1])
   
-  x0 = D_points[1]
+  #Step 1 compute a minimal spanning tree
+  edges = minimal_spanning_tree(D_points)
   
-  minimal_spanning_tree(D_points)
+  #Choose a suitable base point and connect it to the spanning tree
+
+  #Multiple ways to choose the base point. 
+  #This one is most suitable when computing abel-jacobi maps. 
+  #Take some integer point to the left of the point with the smallest real part
+    
+  
+  if abel_jacobi
+    
+    #Real part should already be minimal in D_points
+    x0 = Cc(min(floor(fmpz, real(D_points[1]) - 2*max_radius(RS)), -1))
+    push!(D_points, x0)
+    
+    #Connect base point to closest point in D_points
+    closest = 1
+    distance = abs(x0 - D_points[1])
+    for i in (2:length(D_points))
+      new_distance = abs(x0 - D_points[i])
+      if distance > new_distance
+        closest = i
+        distance = new_distance
+      end
+    end
+    
+    push!(edges, (d +1, closest))
+  else
+  #Here we take the one that is most suitable if one doesn't need to compute Abel-Jacobi maps according to Neurohr, i.e. we split the longest edge in the middle. 
+  #(Last edge should be the longest in the way we compute minimal_spanning trees right now.)
+    
+    left = edges[end][1]
+    right = edges[end][2]
+    pop!(edges)
+    x0 = (D_points[left] + D_points[right])//2
+    push!(D_points, x0)
+    push!(edges, (d + 1, left))
+    push!(edges, (d + 1, right))
+  end
+  
+  #Now we sort the points by angle and level
+
+  paths = CPath[]
+  past_nodes = [d + 1]
+  current_node = d + 1
+  
+  left_edges = filter(t -> t[1] == current_node && !(t[2] in past_nodes), edges)
+  right_edges = filter(t -> t[1] == current_node && !(t[2] in past_nodes), map(reverse,edges))
+  
+  leftright = vcat(left_edges, right_edges)
+    
+  angle_ordering = function(t1::Tuple{Int, Int}, t2::Tuple{Int, Int})
+    return mod2pi(angle(current_node - D_points[t1[2]]) - current_angle) < mod2pi(angle(current_node - D_points[t2[2]]) - current_angle)
+  end
+      
+  sort!(leftright, lt = angle_ordering)
+  
+  paths = vcat(paths, leftright)
+  current_level = vcat(left_edges, right_edges)
+  
+  while length(paths) < length(edges)
+    next_level = []
+    for edge in current_level
+    
+      previous_node = edge[1]
+      current_node = edge[2]
+      current_angle = angle(D_points[previous_node] - D_points[current_node])
+      
+      left_edges = filter(t -> t[1] == current_node && !(t[2] in past_nodes), edges)
+      right_edges = filter(t -> t[1] == current_node && !(t[2] in past_nodes), map(reverse, edges))
+      leftright = vcat(left_edges, right_edges)
+    
+      angle_ordering = function(t1::Tuple{Int, Int}, t2::Tuple{Int, Int})
+        return mod2pi(angle(current_node - D_points[t1[2]]) - current_angle) < mod2pi(angle(current_node - D_points[t2[2]]) - current_angle)
+      end
+      
+      sort!(leftright, lt = angle_ordering)
+      next_level = vcat(next_level, leftright)
+      paths = vcat(paths, leftright)
+      
+      push!(past_nodes, current_node) 
+    end
+    
+    current_level = next_level
+  end
+  
   
 end
 
+function Base.mod2pi(x::arb)
+  pi2 = 2*const_pi(parent(x))
+  while x < 0
+    x += pi2
+  end
+  
+  while x > pi2
+    x -= pi2
+  end
+  
+  return x
+end
 
 #Could be optimized probably. Kruskal's algorithm
 function minimal_spanning_tree(v::Vector{acb})
@@ -104,9 +242,9 @@ function minimal_spanning_tree(v::Vector{acb})
   
   sort!(edge_weights) 
   
-  tree = []
+  tree = Tuple{Int, Int}[]
   
-  nodes = []
+  nodes = Int[]
   
   i = 1
   
@@ -143,9 +281,10 @@ function discriminant_points(RS::RiemannSurface)
     push!(a0_factors, embed_poly(f, v, prec))
   end
   
-  D1 = vcat([roots(fac, initial_prec = prec) for fac in disc_y_factors]...)
-  D2 = vcat([roots(fac, initial_prec = prec) for fac in a0_factors]...)
+  D1 = vcat(acb[],[roots(fac, initial_prec = prec) for fac in disc_y_factors]...)
+  D2 = vcat(acb[],[roots(fac, initial_prec = prec) for fac in a0_factors]...)
   D_points = sort!(vcat(D1, D2), lt = sheet_ordering)
+    @show typeof(D2)
   return D_points
 end
 
